49234e6a2c700e10d0f28fe45878153d665df8c2
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-03 16:26:56 +0000

    number&math notes

---
+++ basics_1/number_math.js
@@ -0,0 +1,45 @@
+// const score=100
+// console.log(score)
+
+// const balance= new Number(2300)
+// console.log(balance)
+// console.log(typeof balance)
+
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2))   // converts into string form 
+
+
+// const buck=123.895
+// console.log(buck.toPrecision(4))  // converts into string form
+
+// const money=1000000
+// console.log(money.toLocaleString())   // converts into string form
+// console.log(money.toLocaleString('en-In'))
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.PI)
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.max(4,3,6))
+// console.log(Math.min(2,7,8))       
+// console.log(Math.random())   // 0-1
+
+console.log((Math.random()*10)+1)
+console.log((Math.floor((Math.random()*10)+1)))
+
+const min=10
+const max=20
+
+console.log(Math.floor(((Math.random())*(max-min+1))+min))
+
+
+
