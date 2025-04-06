b74bc6771c7f336de159792802e4d0a2db7c93a8
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-06 13:14:27 +0000

    api

---
+++ objects/api.js
@@ -0,0 +1,21 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courceInstructor:"Hitesh"
+}
+
+const {courseInstructor:instructor} =course  //destructuring
+console.log(instructor)
+
+
+// {
+ //     "name": "hitesh",                        api's in jason format
+ //     "coursename": "js in hindi",
+ //     "price": "free"
+ // }
+ 
+//  [ 
+//     {},
+//     {},
+//     {}
+// ]
