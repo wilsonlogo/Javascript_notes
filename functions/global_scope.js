771613b8174751f9087a34a0967e883c91de8359
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-06 19:15:08 +0000

    global_scope

---
+++ functions/global_scope.js
@@ -0,0 +1,57 @@
+//var c = 300
+let a = 300                    // global scope
+if (true) {                       // block scope
+    let a = 10
+    const b = 20                     
+    console.log("INNER: ", a);
+    
+}
+// node scope is different from windows scope
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))             // function hoisting
+                                       
+function addone(num){
+    return num + 1
+}
+
+
+                                            
+addTwo(5)                             // function expression not hoisted
+const addTwo = function(num){
+    return num + 2
+}
