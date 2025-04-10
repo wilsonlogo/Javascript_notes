285c42c2d860844913e2a86d76e3d51c55c626c1
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-10 16:45:33 +0000

    while do while loop

---
+++ iterations/while.js
@@ -0,0 +1,25 @@
+
+// let index=0
+// while(index<=10){
+//      console.log(`Value of index is ${index}`)
+//      index=index+2
+// }
+
+// let myArray=["flash","batman","Superman"]
+// let i=0
+// while(i<myArray.length){
+//     console.log(`value is ${myArray[i]}`)
+//     i=i+1
+// }
+
+// let score=1
+// do {
+//     console.log(`value is ${score}`)
+//     score=score+1
+// } while (score<=10);
+
+let score=11
+do {
+    console.log(`score is : ${score}`)
+    score=score+1
+} while (score<=10);
