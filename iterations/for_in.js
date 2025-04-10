5a224802b8aa9236c0136613e46c7ac846dffb2c
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-10 18:43:24 +0000

    for in loop

---
+++ iterations/for_in.js
@@ -0,0 +1,27 @@
+// const myObject={
+//     js:"Javascript",
+//     cpp:"C++",
+//     rb:"Ruby",
+//     swift:"Swift for Apple"
+// }
+
+// for(const keys in myObject){
+//     console.log(`${keys} :- ${myObject[keys]}`)
+// }
+
+// const pro_lan=["cpp","js","java","rb","py"]
+// for(const key in pro_lan){
+//     console.log(pro_lan[key])
+// }
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United States OF America")
+map.set('Fr',"France")
+map.set('IN',"IND")            // keys are unique and are not duplicated 
+map.set('Fr',"France")         // no futher duplications
+
+for(const keys in map){
+    console.log(map[keys])      // not iterable by this method only for of loop will work
+}
+
