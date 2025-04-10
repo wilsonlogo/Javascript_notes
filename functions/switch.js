2c7ba1bbe746476cb77fefa73be8ba7f0a187e6f
wilsonlogo <154533576+wilsonlogo@users.noreply.github.com>
2025-04-10 13:27:55 +0000

    switch

---
+++ functions/switch.js
@@ -0,0 +1,25 @@
+// switch(key){
+//     case 1:
+//         break;
+//     default:
+//         break;
+// }
+
+// const month=3  // "march"
+// switch(month){
+//     case 1:  // "january"
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     default:
+//         console.log("Invalaid month");
+//         break;
+// }
